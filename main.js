95c14d8be786f576d046708e76476909b6eba6ef
Nucci-karim <nuccikarim@gmail.com>
2023-04-26 11:10:08 +0200

    esercizio corretto

--- main.js
+++ main.js
@@ -3,18 +3,22 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-        emailRand: ''
+        emailRand: [
+
+        ]
       }
     },
     created(){
-        
+        for(let i=0; i < 10; i++){
+            this.API()
+        }
     },
     methods: {
         API(){
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(risposta => {
-            this.emailRand = risposta.data.response
+            this.emailRand.push(risposta.data.response)
             })
-        }
+        },
         
     },
   }).mount('#app')
